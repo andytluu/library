ddcfadfc17cf325a0bf79eb3b683ed6326283c70
Andy Luue <155850018+andytluu@users.noreply.github.com>
2024-10-22 20:57:16 -0700

    Added book objects to array

--- library.js
+++ library.js
@@ -1,7 +1,25 @@
-const myLibrary =["GOT","Dragon","Code Geass"];
+const myLibrary =[{
+    title:"GOT",
+    author:"G.R.Martin",
+    pages:200,
+    read:"yes"
+},
+{
+    title:"Aragon",
+    author:"Dragon guy",
+    pages:150,
+    read:"no",
+},
+{
+    title:"Dune",
+    author:"Sand guy",
+    pages:300,
+    read:"yes",
+}];
 const display = document.querySelector("#display");
 const add = document.querySelector("#add");
 const form = document.querySelector("#book-form");
+const formButton = document.querySelector("#form-button")
 function Book(title,author,pages,read) {
     this.title = title;
     this.author = author;
@@ -17,7 +35,7 @@ function Book(title,author,pages,read) {
 }
 
 function addBookToLibrary(){
-
+    
 }
 function showForm(){
     form.style.display = "block";
